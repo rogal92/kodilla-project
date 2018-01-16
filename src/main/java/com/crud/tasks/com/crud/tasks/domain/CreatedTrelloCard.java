9f3b4447b376fc12d18d664f5b8f91f3081389ed
Rogalik <rogalski92@hotmail.com>
2018-01-16 19:08:26 +0100

    18.3 rozbudowa klienta

--- src/main/java/com/crud/tasks/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -18,4 +19,33 @@ public class CreatedTrelloCard {
 
     @JsonProperty("shortUrl")
     private String shortUrl;
+
+    @JsonProperty("badges")
+    private Badges badges;
+
+    @JsonProperty("votes")
+    private int votes;
+
+    @JsonProperty("attachments")
+    private AttachmentsByType attachments;
+
+    @JsonProperty("trello")
+    private Trello trello;
+
+    @JsonProperty("board")
+    private int board;
+
+    @JsonProperty("card")
+    private int card;
+
+    public final static class Badges {
+        private final int votes;
+        private final AttachmentsByType attachments;
+
+        @JsonCreator
+        public Badges(@JsonProperty("votes") int votes, @JsonProperty("attachments") AttachmentsByType attachments) {
+            this.votes = votes;
+            this.attachments = attachments;
+        }
+    }
 }
