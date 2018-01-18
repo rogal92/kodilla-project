271a970bfbd8f3044c8a1c53d9425982014ad1c9
Rogalik <rogalski92@hotmail.com>
2018-01-18 15:34:26 +0100

    First attempt to heroku

--- src/main/java/com/crud/tasks/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/com/crud/tasks/controller/TaskController.java
@@ -3,7 +3,6 @@ package com.crud.tasks.com.crud.tasks.controller;
 import com.crud.tasks.com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.com.crud.tasks.service.DbService;
-import javafx.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
