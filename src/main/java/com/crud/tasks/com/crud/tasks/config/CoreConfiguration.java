67a583bc05f0391535469c155bca44d1654805ba
Rogalik <rogalski92@hotmail.com>
2018-01-20 10:30:35 +0100

    19.2

--- src/main/java/com/crud/tasks/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/com/crud/tasks/config/CoreConfiguration.java
@@ -25,8 +25,8 @@ public class CoreConfiguration implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.ant("/com.crud.tasks.controller/*"))
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
+                .paths(PathSelectors.any())
                 .build();
     }
 
