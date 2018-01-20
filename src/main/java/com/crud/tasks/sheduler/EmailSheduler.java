dee319be633324d2f987331cb80726d761e5e566
Rogalik <rogalski92@hotmail.com>
2018-01-20 17:19:31 +0100

    19.4

---
+++ src/main/java/com/crud/tasks/sheduler/EmailSheduler.java
@@ -0,0 +1,48 @@
+package com.crud.tasks.sheduler;
+
+import com.crud.tasks.com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.com.crud.tasks.domain.Mail;
+import com.crud.tasks.com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.com.crud.tasks.service.TrelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
+
+import javax.security.auth.Subject;
+
+public class EmailSheduler {
+
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void oneTaskMail() {
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                SUBJECT,
+                "Currently in database you've got " + size + " task"
+        ));
+    }
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        if (size == 1) {
+            oneTaskMail();
+        } else {
+            simpleEmailService.send(new Mail(
+                    adminConfig.getAdminMail(),
+                    SUBJECT,
+                    "Currently in database you've got " + size + " tasks"
+            ));
+        }
+        @Scheduled(fixedDelay = "10000") ;
+    }
+}
