91d74899bb866f192cb82fdeed6b14cda6855ebf
Rogalik <rogalski92@hotmail.com>
2018-02-28 12:13:01 +0100

    Application.properties

--- src/main/java/com/crud/tasks/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/com/crud/tasks/controller/StaticWebPageController.java
@@ -1,4 +1,23 @@
 package com.crud.tasks.com.crud.tasks.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
 public class StaticWebPageController {
+
+    @RequestMapping("/")
+    public String index(Map<String,Object> model) {
+        model.put("variable","My thymeleaf variable");
+        model.put("four",4);
+        model.put("two",2);
+        model.put("multiply","*");
+        model.put("equals","=");
+        model.put("plus","+");
+        model.put("minus","-");
+        model.put("-two",-2);
+        return "index";
+    }
 }
